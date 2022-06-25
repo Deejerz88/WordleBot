00287de408c050b3c6dfbd552daf6ad1474857e5
Deejerz88 <djmcmillan93@gmail.com>
2022-06-25 09:02:07 -0400

    formatting stuff

--- commands/wordle.js
+++ commands/wordle.js
@@ -202,32 +202,32 @@ ${golfStr}
             }
           })
           .then(async () => {
-            // let newValues = {
-            //   $set: {
-            //     games: numGames,
-            //     average: avg,
-            //     jamesScore: jamesScore,
-            //     distribution: dist,
-            //     wordleGolf: gScores,
-            //   },
-            // };
-            // await mongoose
-            //   .connect(process.env.MONGO_URI)
-            //   .then(() => console.log("MongoDB has been connected"))
-            //   .catch((error) => {
-            //     console.log(error);
-            //   });
-            // const db = mongoose.connection;
-            // const collection = db.collection("Wordle");
-            // collection.updateOne(
-            //   { user: `${username}-${id}` },
-            //   newValues,
-            //   { upsert: true },
-            //   (err, res) => {
-            //     console.log(res);
-            //     if (err) throw err;
-            //   }
-            // );
+            let newValues = {
+              $set: {
+                games: numGames,
+                average: avg,
+                jamesScore: jamesScore,
+                distribution: dist,
+                wordleGolf: gScores,
+              },
+            };
+            await mongoose
+              .connect(process.env.MONGO_URI)
+              .then(() => console.log("MongoDB has been connected"))
+              .catch((error) => {
+                console.log(error);
+              });
+            const db = mongoose.connection;
+            const collection = db.collection("Wordle");
+            collection.updateOne(
+              { user: `${username}-${id}` },
+              newValues,
+              { upsert: true },
+              (err, res) => {
+                console.log(res);
+                if (err) throw err;
+              }
+            );
           });
 
         console.log("replied");
